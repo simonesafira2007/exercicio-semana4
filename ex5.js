dd4da0a65e01b8fb1b0f0c56286ca485c509c541
simonesafira2007 <simone.safira2007@gmail.com>
2020-11-05 16:32:50 -0300

    correção

--- ex5.js
+++ ex5.js
@@ -1,6 +1,6 @@
 const input = require("readline-sync")
 /*
-Escreva um programa que recebe um valor inserido pelo usuário e imprime na tela os números até o valor digitado. S
+Escreva um programa que recebe um valor inserido pelo usuário e imprime na tela os números até o valor digitado. 
 */
 const number = input.question("Digite um numero para o valor final do loop : ")
 
