a4739a18798e5a1dfd00a838ddf939a8a35463d6
simonesafira2007 <simone.safira2007@gmail.com>
2020-11-05 16:04:15 -0300

    ex4.js resolvido

---
+++ ex4.js
@@ -0,0 +1,24 @@
+/*
+Escreva um algoritmo que imprima os números (inteiros e positivos) menores que 100,
+de 10 em 10.
+*/
+
+for( let i = 1; i <= 100; i++){
+    if (i % 10 == 0){ // modificando o valor do divisor, verificam-se outras saídas possíveis
+        console.log(i)
+    }
+    
+}
+
+/*
+10
+20
+30
+40
+50
+60
+70
+80
+90
+100
+*/
