950f359f08952e438294485b2fae9199d6d61509
simonesafira2007 <simone.safira2007@gmail.com>
2020-11-05 18:16:17 -0300

    alterações

--- ex6.js
+++ ex6.js
@@ -1,14 +1,15 @@
 const input = require('readline-sync')
 
-// Programa que recebe um número digitado pelo usuário e imprime a Tabuada do número informado
+// Programa que recebe um número de 1 a 10 digitado pelo usuário e imprime a Tabuada do número informado.
+// Todo número multiplicado por zero é igual a zero. Por isso, não o incluímos.
 
-const num = input.question("Informe um numero de 1 a 10 : ")
-if (num > 10) {
+const num = input.question("Informe um numero ente 1 a 10 : ")
+if (num > 10 || num <= 0){
     console.log("     === Opcao inválida ====")
     console.log(" === Só aceita número de 1 a 10 ===")
 } else {
 
-for(let i = 1 ; i <= 10; i++){
+for(let i = 1 ; i <= 10; i++)
     console.log( num, "X",  i , " = " , num * i  )
 }
-}
+
---
+++ ex7.js
@@ -0,0 +1,6 @@
+const input = require("readline-sync")
+/*
+Escreva um algoritmo para ler 10 valores, calcular e escrever a média aritmética desses
+valores.
+*/
+
