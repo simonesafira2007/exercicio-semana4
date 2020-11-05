0966c39ef3e071b25671e17178903137545eb633
simonesafira2007 <simone.safira2007@gmail.com>
2020-11-05 15:39:48 -0300

    exercícios ex2.js e ex3.js

---
+++ ex2.js
@@ -0,0 +1,19 @@
+/*
+Escreva um algoritmo que imprima os números pares no intervalo de 0 a 10 em
+ordem crescente.
+*/
+
+for( let i = 1; i <= 10; i++){
+    if (i % 2 != 0)
+    console.log(i)
+}
+
+/*
+Escreva um algoritmo que imprima os números pares no intervalo de 0 a 10 em
+ordem decrescente.
+*/
+
+for( let i = 10; i <= 1; i--){
+    if (i % 2 == 0)
+    console.log(i)
+}
---
+++ ex3.js
@@ -0,0 +1,10 @@
+
+/*
+Escreva um algoritmo que imprima os números pares no intervalo de 0 a 10 em
+ordem decrescente.
+*/
+
+for( let i = 1; i <= 10; i++){
+    if (i % 2 == 0)
+    console.log(i)
+}
