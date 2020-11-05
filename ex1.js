2bedf2c78c282c6e3b7f08204f4ac44d99b4859a
simonesafira2007 <simone.safira2007@gmail.com>
2020-11-05 15:28:03 -0300

    ex1.js resolvido

---
+++ ex1.js
@@ -0,0 +1,43 @@
+/*
+Escreva um algoritmo que imprima os números de 1 (inclusive) a 10 (inclusive) em
+ordem crescente.
+*/
+
+for (let i = 1; i <= 10 ; i++){
+    console.log(i)
+    
+}
+ 
+/*
+Escreva um algoritmo que imprima os números de 1 (inclusive) a 10 (inclusive) em
+ordem decrescente.
+*/
+
+for (let i = 10; i >= 1 ; i--){
+    console.log(i)
+    
+}
+ 
+/*
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+
+*/
