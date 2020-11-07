3c68b63decb1582f19418b8ffdcb09d9bf3c5d57
simonesafira2007 <simone.safira2007@gmail.com>
2020-11-07 15:21:49 -0300

    acréscimos

--- triangulo.js
+++ triangulo.js
@@ -6,9 +6,9 @@ Triângulo escaleno:   todos os lados possuem medidas diferentes;
 Isósceles : quando dois lados possuem a mesma medida; 
 Equilátero, quando todos os lados são iguais.
 */
-const A = input.question("Entre com valor do primeiro lado")
-const B = input.question("Entre com valor do segundo lado")
-const C = input.question("Entre com valor do terceiro lado")
+const A = input.question("Entre com valor do primeiro lado :")
+const B = input.question("Entre com valor do segundo lado :")
+const C = input.question("Entre com valor do terceiro lado :")
 
 if ( A == B  &   A == C   &   B == C ) {
     console.log("EQUILATERO")
@@ -19,3 +19,19 @@ if ( A == B  &   A == C   &   B == C ) {
     )
 
 }
+/*
+Entre com valor do primeiro lado :6
+Entre com valor do segundo lado :7
+Entre com valor do terceiro lado :2
+ESCALENO
+
+Entre com valor do primeiro lado :5
+Entre com valor do segundo lado :5
+Entre com valor do terceiro lado :5
+EQUILATERO
+
+Entre com valor do primeiro lado :6
+Entre com valor do segundo lado :6
+Entre com valor do terceiro lado :4
+ISOCELES
+*/
