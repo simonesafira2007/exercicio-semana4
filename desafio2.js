33658c76bd7439df81bc18733ed824588e9f34b0
simonesafira2007 <simone.safira2007@gmail.com>
2020-11-09 21:40:41 -0300

    alterações

--- desafio2.js
+++ desafio2.js
@@ -1,9 +1,10 @@
 
 
 /*
- Exiba na tela todos a frase, porém de maneira invertida (do último para o primeiro).
+ Exiba na tela a frase armazenada na constante, porém de maneira invertida (do último para o primeiro).
 */
 
 
 const frase = 'TUDO PASSA, MAS O AMOR PERMANECE'.split('').reverse().join('')
 console.log(frase)
+// ECENAMREP ROMA O SAM ,ASSAP ODUT
