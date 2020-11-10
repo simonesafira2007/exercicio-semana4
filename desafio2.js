4b88fe45fc0ce82550fd7ce0ac8e10a4876cb896
simonesafira2007 <simone.safira2007@gmail.com>
2020-11-09 21:33:31 -0300

    exercício desafio2,js

---
+++ desafio2.js
@@ -0,0 +1,9 @@
+
+
+/*
+ Exiba na tela todos a frase, porém de maneira invertida (do último para o primeiro).
+*/
+
+
+const frase = 'TUDO PASSA, MAS O AMOR PERMANECE'.split('').reverse().join('')
+console.log(frase)
